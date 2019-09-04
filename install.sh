af33aa628888247c535413658d809304969f5a94
blb78 <blb78@pm.me>
2019-09-04 14:40:59 +0200

    refacto install

--- install.sh
+++ install.sh
@@ -1,71 +1,30 @@
 update_arch(){
 	yay
 }
-install_alacritty(){
-	if ! type alacritty > /dev/null; then
-		echo "installing alacritty"
-		sudo pacman -Sy alacritty
+install_ohmyzsh(){
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+generate_ssh(){
+	read -p "Enter your github email: " email
+	ssh-keygen -t rsa -b 4096 -C "$email"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
+}
+
+run_pacman(){
+	if ! type $1 > /dev/null; then
+		echo -e "\e[33minstalling $1\e[0m"
+		sudo pacman -Sy $1
 	else
-		echo "alacritty is already installed"
+		echo -e "\e[32m$1 is already installed\e[0m"
 	fi
 }
-install_betterlockscreen(){
-	if ! type betterlockscreen > /dev/null; then
-		echo "installing betterlockscreen"
-		sudo yay -Sy betterlockscreen
+run_yay(){
+	if ! type $1 > /dev/null; then
+		echo -e "\e[33minstalling $1\e[0m"
+		yay -Sy $1
 	else
-		echo "betterlockscreen is already installed"
-	fi
-}
-install_curl(){
-	if ! type curl > /dev/null; then
-		echo "installing curl"
-		sudo pacman -Sy curl
-	else
-		echo "curl is already installed"
-	fi
-}
-install_nvim(){
-	if ! type nvim > /dev/null; then
-		echo "installing neovim"
-		sudo pacman -Sy neovim
-	else
-		echo "neovim is already installed"
-	fi
-}
-install_rofi(){
-	if ! type rofi > /dev/null; then
-		echo "installing rofi"
-		sudo pacman -Sy rofi
-	else
-		echo "rofi is alacritty installed"
-	fi
-}
-install_tmux(){
-	if ! type tmux > /dev/null; then
-		echo "installing tmux"
-		sudo pacman -Sy tmux
-	else
-		echo "tmux is already installed"
-	fi
-}
-install_vim(){
-	if ! type vim > /dev/null; then
-		echo "installing vim"
-		sudo pacman -Sy vim
-	else
-		echo "vim is already installed"
-	fi
-}
-install_zsh(){
-	if ! type zsh > /dev/null;then
-		echo "installing zsh"
-		sudo pacman -Sy zsh
-		sudo chsh -s $(which zsh)
-		# install ohmyzsh
-		sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	else
-		echo "zsh is already installed"
+		echo -e "\e[32m$1 is already installed\e[0m"
 	fi
 }
 ###############################################################################
@@ -80,18 +39,42 @@ select yn in "Yes" "No"; do
 		No ) break;;
 	esac
 done
-# Declare an array of string with type
-declare -a Apps=("alacritty" "betterlockscreen" "curl" "nvim" "rofi" "tmux" "vim" "zsh" )
-# Iterate the string array using for loop
-for val in ${Apps[@]}; do
-	echo "Do you wish to update $val ?"
+declare -a PacmanApps=("alacritty" "curl" "nvim" "openssh" "powerline" "powerline-fonts" "rofi" "tmux" "vim" "wget" "xclip" "zsh")
+for val in ${PacmanApps[@]}; do
+	echo "Do you wish to install $val ?"
 	select yn in "Yes" "No"; do
 		case $yn in
-			Yes ) install_$val; break;;
+			Yes ) run_pacman $val; break;;
 			No ) break;;
 		esac
 	done
 done
+echo "Do you wish to install OhMyZsh ?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) install_ohmyzsh; break;;
+		No ) break;;
+	esac
+done
+declare -a YayApps=("betterlockscreen")
+for val in ${YayApps[@]}; do
+	echo "Do you wish to install $val ?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) run_yay $val; break;;
+			No ) break;;
+		esac
+	done
+done
+# Generate SSH key on github
+echo "Do you wish to generate github's ssh key ?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) generate_ssh; break;;
+		No ) break;;
+	esac
+done
+
 
 # ln ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 # ln ./betterlockscreen/betterlockscreenrc ~/.config/betterlockscreen/betterlockscreenrc
