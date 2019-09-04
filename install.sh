742ee077241b813a19489bb7a2111cfe4fbe220f
blb78 <blb78@pm.me>
2019-09-04 14:51:12 +0200

    fix remove yay folder

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ install_yay(){
 	cd yay
 	makepkg -si
 	cd ..
-	rmdir yay/
+	rm -rf yay/
 }
 install_ohmyzsh(){
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
